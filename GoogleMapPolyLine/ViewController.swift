1ad8b4bc92b7a22599774e2fac1b13ba7c572aa6
boominadhaprakash <boomi.m@dunstconsulting.in>
2017-04-25 15:17:56 +0530

    Added code to display the map center between the source and destination markers

--- GoogleMapPolyLine/ViewController.swift
+++ GoogleMapPolyLine/ViewController.swift
@@ -100,6 +100,22 @@ class ViewController: UIViewController, GMSMapViewDelegate {
         self.destinationMarker.icon = GMSMarker.markerImage(with: UIColor.red)
         self.destinationMarker.title = self.destinationAddress
         
+        let point1:MKMapPoint = MKMapPointForCoordinate(originMarker.position)
+        let point2:MKMapPoint = MKMapPointForCoordinate(destinationMarker.position)
+        
+        let mapViewWidth = self.gmapview.frame.size.width
+        let mapViewHeight = self.gmapview.frame.size.height
+        
+        let centerPoint = MKMapPointMake((point1.x + point2.x) / 2, (point1.y + point2.y)/2)
+        let centerLocation:CLLocationCoordinate2D = MKCoordinateForMapPoint(centerPoint)
+        
+        let mapScaleWidth:Double = Double(Double(mapViewWidth) / Double(fabs(point2.x - point1.x)))
+        let mapScaleHeight:Double = Double(Double(mapViewHeight) / Double(fabs(point2.y - point1.y)))
+        let mapScale = min(mapScaleWidth, mapScaleHeight)
+        
+        let zoomLevel = 19 + log2(mapScale)
+        self.gmapview.camera = GMSCameraPosition.camera(withLatitude: centerLocation.latitude, longitude: centerLocation.longitude, zoom: Float(zoomLevel))
+        
         let path = GMSMutablePath(fromEncodedPath: encodedString)
         let polyline = GMSPolyline(path: path)
         polyline.strokeWidth = 5
